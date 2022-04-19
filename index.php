1963eb177950a32a188443b2ed714edc73015da7
Laura Savickaite <laura.savickaite@laplateforme.io>
2022-04-19 11:04:24 +0200

    images + database connection

---
+++ index.php
@@ -0,0 +1,27 @@
+<!-- Cette page doit ressembler à la page d’accueil d’un moteur de recherche. -->
+
+<?php
+
+if(isset($_POST['search']))
+{
+    var_dump($_POST['search']);   
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autocompletion</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="search" placeholder="Search">
+        <button id="searchbutton" type="submit" name="submit-search">Search</button>
+    </form>    
+</body>
+</html>
+
